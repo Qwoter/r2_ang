cac8588e3add6c9a0be89d324948e28b5b8f6240
Nikita Kovalyov <qwoter@gmail.com>
2014-10-28 16:48:15 +0200

    static assets on

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module R2Ang
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
+    config.serve_static_assets = true
   end
 end
